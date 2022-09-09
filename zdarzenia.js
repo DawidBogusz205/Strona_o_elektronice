f5dc6f0b4f9e4b5921118d810dde088d6ba35900
BugiBugi205 <dawid2005bugi@gmail.com>
2022-09-09 19:18:43 +0200

    dodano formularz kontaktowy do osobnej strony
    zamiast w footerze całość formularza

--- zdarzenia.js
+++ zdarzenia.js
@@ -11,17 +11,6 @@ function default_change(element){
     }
 }
 
-function default_change_only_class(element){
-    if(element.className == "on")
-    {
-        element.className="off";
-    }
-    else
-    {
-        element.className="on";
-    }
-}
-
 document.getElementById("Click").addEventListener("click",function(){
     default_change(this);
 });
@@ -51,17 +40,17 @@ document.getElementById("Mousemove").addEventListener("mousemove",function(){
 });
 
 document.getElementById("Change").addEventListener("change",function(){
-    default_change_only_class(this);
+    default_change(this);
 });
 
 document.getElementById("Fokus").addEventListener("focus",function(){
-    default_change_only_class(this);
+    default_change(this);
 });
 
 document.getElementById("Blur").addEventListener("blur",function(){
-    default_change_only_class(this);
+    default_change(this);
 });
 
 document.getElementById("Sel").addEventListener("select",function(){
-    default_change_only_class(this);
+    default_change(this);
 });
